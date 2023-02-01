720962d8867201123375ad99233d323ff0ca306c
adantas- <adantas-@student.42sp.org.br>
2023-02-01 17:17:10 -0300

    1

---
+++ libft/includes/libft_defines.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_defines.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 10:53:10 by adantas-          #+#    #+#             */
+/*   Updated: 2023/01/31 10:54:21 by adantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_DEFINES_H
+# define LIBFT_DEFINES_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# define HEX_L "0123456789abcdef"
+# define HEX_U "0123456789ABCDEF"
+
+#endif
---
+++ push_swap.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/01 15:36:58 by adantas-          #+#    #+#             */
+/*   Updated: 2023/02/01 17:16:34 by adantas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#define MAX_INT 2147483647
+#define MIN_INT -2147483648
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		exit(22);
+	if (argc == 2)
+		exit(0);
+	check_args(argc, argv);
+}
+
+void	check_args(int argc, char **argv)
+{
+	int	i;
+	int	j;
+	ssize_t	
+}
