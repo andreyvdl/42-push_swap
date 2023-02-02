300d65faf2e131e41e136ddd8faf529c98ac873d
adantas- <adantas-@student.42sp.org.br>
2023-02-02 17:53:59 -0300

    home

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:36:58 by adantas-          #+#    #+#             */
-/*   Updated: 2023/02/01 17:16:34 by adantas-         ###   ########.fr       */
+/*   Updated: 2023/02/02 17:20:46 by adantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,123 @@
 
 int	main(int argc, char **argv)
 {
+	t_stack	stks;
+
 	if (argc == 1)
-		exit(22);
-	if (argc == 2)
 		exit(0);
-	check_args(argc, argv);
+	stks = validate(argc, argv);
+	if (argc == 2)
+		return (free_strct(&stks), exit(0));
+	else if (argc == 3)
+		bi_sort(&stks);
+	else if (argc == 4)
+		boog
+	return (free_strct(&stks), exit(0));
 }
 
-void	check_args(int argc, char **argv)
+t_stack	*validate(int size, char **args)
+{
+	int		i;
+	int		j;
+	int		comp;
+	t_stack	stks;
+
+	stks.a = (int *)ft_calloc(argc, sizeof(int));
+	stks.b = (int *)ft_calloc(argc, sizeof(int));
+	stks.f = (int *)ft_calloc(argc, sizeof(int));
+	if (!stks.a || !stks.b || !stks.f)
+		return (free_strct(&stks), msg(12));
+	i = 1;
+	while (i < size - 1)
+	{
+		arg_is_invalid(args[i], &stks);
+		arg_is_over(args[i], &stks);
+		comp = ft_atoi(args[i]);
+		stks.a[i - 1] = (int)comp;
+		i++;
+	}
+	return (&stks);
+}
+
+int	arg_is_over(char *arg, t_stack *stks)
 {
 	int	i;
+	int	comp;
+
+	i = 0;
+	comp = 1;
+	if (arg[i] == '-' || arg[i] == '+')
+	{
+		if (arg[i] == '-')
+			comp = 0;
+		i++;
+	}
+	if (comp)
+		arg_is_big(arg, stks, i);
+	else
+		arg_is_small(arg, stks, i);
+	return (0);
+}
+
+int	arg_is_big(char *arg, t_stack *stks, int i)
+{
 	int	j;
-	ssize_t	
+
+	j = 0;
+	while (arg[i])
+	{
+		if (arg[i] > MAX_INT[j])
+			return (free_strct(stks), msg(22));
+		i++;
+		j++;
+	}
+	return (0);
+}
+
+int	arg_is_small(char *arg, t_stack *stks, int i)
+{
+	int	j;
+
+	j = 1;
+	while (arg[i])
+	{
+		if (arg[i] > MIN_INT[j])
+			return (free_strct(stks), msg(22));
+		i++;
+		j++;
+	}
+	return (0);
+}
+
+int	arg_is_invalid(char *arg, t_stack *stks)
+{
+	int	i;
+
+	i = 0;
+	if (arg[i] == '-' || arg[i] == '+')
+		i++;
+	while (arg[i])
+	{
+		if (!ft_isdigit(arg[i]))
+			return (free_strct(stks), msg(22));
+		i++;
+	}
+	return (0);
+}
+
+int	msg(int code)
+{
+	if (code == 12)
+		return (ft_println("Error: ENOMEM: Cannot allocate memory."),
+			exit(code));
+	else if (code == 22)
+		return (ft_println("Error"), exit(code));
+	return (0);
+}
+
+void	free_strct(t_stack *stks)
+{
+	ft_free(stks->a);
+	ft_free(stks->b);
+	ft_free(stks->f);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: adantas- <adantas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 13:57:04 by adantas-          #+#    #+#             */
-/*   Updated: 2023/02/01 15:36:57 by adantas-         ###   ########.fr       */
+/*   Updated: 2023/02/02 16:02:28 by adantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 # define PUSH_SWAP_H
 
 # include "libft/includes/libft.h"
+
+typedef struct s_stack
+{
+	int	*a;
+	int	*b;
+	int	*f;
+}	t_stack;
 
 #endif
